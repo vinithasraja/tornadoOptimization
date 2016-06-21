0b5af33dad10cd91a3176ebc73e132de46843796
vinithasraja <snellhome1@gmail.com>
2016-06-20 17:14:57 -0700

    Implement taco Handler middleware

--- handlers.py
+++ handlers.py
@@ -8,4 +8,17 @@ class MainHandler(BaseRequestHandler):
         kwargs = {
             'tacos': tacos
         }
-        self.render("index.html", title="Wake Testing Project", **kwargs)
+        self.render("static/taco.html", title="Wake Testing Project", tacos=tacos)
+
+
+class TacoHandler(BaseRequestHandler):
+    def get(self, tacoid):
+        tacos = self.db.query(models.Taco).filter_by(id=tacoid).one()
+        good_toppings = tacos.toppings(good=True)
+        bad_toppings = tacos.toppings(good=False)
+        taco_toppings = {
+          'good_toppings': good_toppings,
+          'bad_toppings': bad_toppings,
+          'name': tacos.name
+        }
+        self.write(json.dumps(taco_toppings))
